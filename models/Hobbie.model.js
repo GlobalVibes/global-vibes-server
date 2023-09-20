c966b53173f78f42619bf75be834354ae1749242
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-20 14:33:27 +0200

    Add Hobbie model

---
+++ models/Hobbie.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+      unique: true,      
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+    },    
+  },
+
+  {   
+    timestamps: true,
+  }
+);
+
+const Hobbie = model("Hobbie", userSchema);
+
+module.exports = Hobbie;
