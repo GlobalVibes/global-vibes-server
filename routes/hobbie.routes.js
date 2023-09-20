23b69e6260b54f695bd24bbf77101e3361cddfd0
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-20 14:58:49 +0200

    Add hobbie routes

---
+++ routes/hobbie.routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router(); 
+const mongoose = require("mongoose");
+
+
+const Hobbie = require("../models/Hobbie.model");
+
+ router.post("/hobbies", (req, res, next) => {
+  const { title, description } = req.body;
+ 
+  Hobbie.create({ title, description})
+    .then(response => res.json(response))
+    .catch(err => res.json(err));
+});
+ 
+module.exports = router;
