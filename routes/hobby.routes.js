9f3c4347294a50c8165642919a561e9286812c5f
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-28 11:23:11 +0200

    improve error handling

--- routes/hobby.routes.js
+++ routes/hobby.routes.js
@@ -7,7 +7,13 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 router.get("/hobbies", (req, res, next) => {
     Hobby.find()
         .then((allHobbies) => res.json(allHobbies))
-        .catch(e => console.log(e));
+        .catch(err => {
+            console.log("Error getting list of hobbies...", err);
+            res.status(500).json({
+                message: "Error getting list of hobbies",
+                error: err
+            });
+        });
 })
 
 router.post("/hobbies", isAuthenticated, (req, res, next) => {
@@ -15,7 +21,13 @@ router.post("/hobbies", isAuthenticated, (req, res, next) => {
 
     Hobby.create({ title })
         .then(response => res.json(response))
-        .catch(err => res.json(err));
+        .catch(err => {
+            console.log("Error creating new hobby...", err);
+            res.status(500).json({
+                message: "Error creating a new hobby",
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,7 +10,13 @@ router.get("/posts", (req, res, next) => {
     Post.find()
         .populate("hobby") 
         .then((allPosts) => res.json(allPosts))
-        .catch(e => console.log(e));
+        .catch(err => {
+            console.log("Error geting list of posts...", err);
+            res.status(500).json({
+                message: "Error geting list of posts",
+                error: err
+            });
+        });
 });
 
 router.post("/posts", isAuthenticated, (req, res, next) => { 
@@ -25,7 +31,13 @@ router.post("/posts", isAuthenticated, (req, res, next) => {
 
     Post.create(newPost)
         .then(post => res.json(post))
-        .catch(e => console.log(e));
+        .catch(err => {
+            console.log("Error creating posts...", err);
+            res.status(500).json({
+                message: "Error creating posts",
+                error: err
+            });
+        });
 });
 
 router.get("/posts/:id", (req, res) => {
@@ -39,12 +51,24 @@ router.put("/posts/:id", isAuthenticated, (req, res, next) => {
         .then((updatedPost) => {
             res.json(updatedPost)
         })
-        .catch(e => console.log(e));
+        .catch(err => {
+            console.log("Error geting details of post...", err);
+            res.status(500).json({
+                message: "Error geting details of post",
+                error: err
+            });
+        });
 });
 
 router.delete("/posts/:id", isAuthenticated, (req, res, next) => { 
     Post.findByIdAndDelete(req.params.id)
      .then(() => res.status(200).json())
-     .catch(e => console.log(e))
+     .catch(err => {
+        console.log("Error deleting post...", err);
+        res.status(500).json({
+            message: "Error deleting post",
+            error: err
+        });
+    });
 })
 module.exports = router;
